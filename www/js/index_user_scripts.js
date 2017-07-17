f61a3a45b618400cb142cfabf38506309063d6a0
mrp3t0k <fauzan.psp@gmail.com>
2017-07-17 16:29:27 +0800

    Add files via upload

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,87 @@
+/*jshint browser:true */
+/*global $ */(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* button  Back */
+    
+    
+        /* button  .uib_w_1 */
+    $(document).on("click", ".uib_w_1", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#info"); 
+         return false;
+    });
+    
+        /* button  .uib_w_3 */
+    $(document).on("click", ".uib_w_3", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#ourservices"); 
+         return false;
+    });
+    
+        /* button  .uib_w_4 */
+    $(document).on("click", ".uib_w_4", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#Galeri"); 
+         return false;
+    });
+    
+        /* button  .uib_w_7 */
+    $(document).on("click", ".uib_w_7", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_9 */
+    $(document).on("click", ".uib_w_9", function(evt)
+    {
+         /*global uib_sb */
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_8"));  
+         return false;
+    });
+    
+        /* button  .uib_w_13 */
+    $(document).on("click", ".uib_w_13", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_14 */
+    $(document).on("click", ".uib_w_14", function(evt)
+    {
+         /*global uib_sb */
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_16"));  
+         return false;
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
